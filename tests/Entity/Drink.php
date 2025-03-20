5a51aa36740f07eb0f5506ebda0d83c9b679a1ea
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-20 16:38:55 +0700

    add Drink.php

---
+++ tests/Entity/Drink.php
@@ -0,0 +1,18 @@
+<?php 
+
+    namespace Cafetaria\Helper\Entity;
+
+    class Drink 
+    {
+        private string $name;
+        private int $price;
+
+        public function __construct(string $name, int $price)
+        { 
+            $this->name = $name; 
+            $this->price = $price;
+        }
+
+        public function getName(): string{ return $this->name; }
+        public function getPrice(): int{  return $this->price; }
+    }
