fb854a6bc1d9f8e580e8c894e2285bf762ab32da
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-20 17:16:05 +0700

    add CheckHelperTest.php add testFoodFoundExactMatch

---
+++ tests/CheckHelperTest.php
@@ -0,0 +1,23 @@
+<?php 
+
+    namespace Cafetaria\Helper;
+
+    use PHPUnit\Framework\TestCase;
+    use PHPUnit\Framework\Assert;
+    use Cafetaria\Helper\Entity\Food;
+    use Cafetaria\Helper\Entity\Drink;
+
+    class CheckHelperTest extends TestCase
+    {
+        public function testFoodFoundExactMatch()
+        {
+            $foods = [
+                new Food("Mie Ayam", 6000),
+                new Food("Ayam Goreng", 12000),
+                new Food("Rawon", 12000)
+            ];
+
+            $result = CheckHelper::check($foods, "Mie Ayam");
+            $this->assertTrue($result);
+        }
+    }
