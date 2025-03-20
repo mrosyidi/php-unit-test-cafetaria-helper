8742b55273f48c15c3f15eb2983429c5fa179d8a
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-20 17:05:55 +0700

    add InputHelperTest.php add testInputWithStream

--- tests/InputHelperTest.php
+++ tests/InputHelperTest.php
@@ -17,4 +17,20 @@
             $this->expectOutputString("Nama: Muhammad Rosyidi\n");
             $this->assertEquals("Muhammad Rosyidi", $result);
         }
+
+        public function testInputWithStream()
+        {
+            $info = "Nama";
+            $simulatedStream = "Muhammad Rosyidi\n";
+
+            $inputStream = fopen('php://memory', 'r+');
+            fwrite($inputStream, $simulatedStream);
+            rewind($inputStream);
+
+            $result = InputHelper::input($info, null, $inputStream);
+            fclose($inputStream);
+
+            $this->expectOutputString("Nama: ");
+            $this->assertEquals("Muhammad Rosyidi", $result);
+        }
     }
