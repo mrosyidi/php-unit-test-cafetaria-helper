e1a67c1ab3a1807545b3680595899cb2c3eb1b1f
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-04-28 14:49:57 +0700

    update RangeHelperTest.php add testRangeNumberEqualsCount

--- tests/RangeHelperTest.php
+++ tests/RangeHelperTest.php
@@ -18,4 +18,15 @@
 
             $this->assertTrue(RangeHelper::range($items, 2));
         }
+
+        public function testRangeNumberEqualsCount()
+        {
+            $items = [
+                new Drink("Jus Jeruk", 5000),
+                new Drink("Es Oyen", 12000),
+                new Drink("Es Campur", 12000)
+            ];
+
+            $this->assertTrue(RangeHelper::range($items, 3));
+        }
     }
