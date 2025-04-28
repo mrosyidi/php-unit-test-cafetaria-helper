e17ff8d96c50b65633c03a31016aee7bdb021d0a
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-04-28 14:46:35 +0700

    add RangeHelperTest.php add testRangeNumberWithingRange

---
+++ tests/RangeHelperTest.php
@@ -0,0 +1,21 @@
+<?php   
+
+    namespace Cafetaria\Helper;
+
+    use PHPUnit\Framework\TestCase;
+    use Cafetaria\Helper\Entity\Food;
+    use Cafetaria\Helper\Entity\Drink;
+
+    class RangeHelperTest extends TestCase
+    {
+        public function testRangeNumberWithinRange()
+        {
+            $items = [
+                new Food("Mie Goreng", 6000),
+                new Food("Pastel", 5000),
+                new Food("Rawon", 12000)
+            ];
+
+            $this->assertTrue(RangeHelper::range($items, 2));
+        }
+    }
