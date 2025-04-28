a2ba2812f7ddd0477f186ac877c8df9796970401
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-04-28 16:13:57 +0700

    add DataHelperTest.php add testDataReturnsCorrectArray

---
+++ tests/DataHelperTest.php
@@ -0,0 +1,23 @@
+<?php   
+
+    namespace Cafetaria\Helper;
+
+    use PHPUnit\Framework\TestCase;
+    use Cafetaria\Helper\Entity\Food;
+    use Cafetaria\Helper\Entity\Drink;
+
+    class DataHelperTest extends TestCase
+    {
+        public function testDataReturnsCorrectArray()
+        {
+            $items = [
+                new Food("Mie Goreng", 6000),
+                new Food("Ayam Panggang", 12000),
+                new Food("Pastel", 6000)
+            ];
+
+            $result = DataHelper::data($items, 1);
+
+            $this->assertEquals(["name" => "Ayam Panggang", "price" => 12000], $result);
+        }
+    }
